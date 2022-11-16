ce5087f753032393ee54ac6664a0acaa55586a93
Michael Crandall <mec192@gmail.com>
2022-11-16 17:19:18 -0600

    added template js with inquirer prompts for manager

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
 const Manager = require('./lib/Manager');
+
+const templates = require('./src/templates')
+
+const DIST = path.resolve(__dirname, 'dist');
+const PATH = path.join(DIST, 'team.html');
 
 const team = [];
 
 function main() {
-    createManager()
-}
+    createManager().then(answers => {
+        const { managerName, managerID, managerEmail, managerOfficeNumber } = answers;
+        const manager = new Manager(
+            managerName,
+            managerID,
+            managerEmail,
+            managerOfficeNumber,
+
+        )
+        team.push(manager)
+    });
+};
 
 function createManager() {
     console.log('Let\'s start building your team');
-    inquirer
+    return inquirer
         .prompt([
             {
                 type: 'input',
@@ -33,17 +50,42 @@ function createManager() {
                 message: 'What is the manager\'s office?'
             }
         ])
-        .then(answers => {
-            const { managerName, managerID, managerEmail, managerOfficeNumber } = answers
-            const manager = new Manager(
-                managerName,
-                managerID,
-                managerEmail,
-                managerOfficeNumber
-            )
+        
+}
 
-            team.push(manager)
-        })
+function createTeam(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'teamMemberChoice',
+            message: 'Which type of team member would you like to add?',
+            choices: ['Engineer', 'Intern', 'I\'ve finished my team']
+        }
+    ]).then(answer => {
+        switch (answer.teamMemberChoice) {
+            case 'Engineer':
+                break;
+            case 'Intern':
+                break;
+            default: 
+                buildTeam();
+        }
+    })
+}
+
+function createHTML(teamArr) {
+    const htmlMain = ``;
+
+    teamArr.foreach(teamMember => {
+        if(teamMember.getRole() === "manager") {
+            htmlMain = htmlMain + templates.managerTemplate(teamMember)
+        }
+    })
+}
+
+
+function buildTeam() {
+    fs.writeFileSync(PATH, createHTML(team), 'utf-8')
 }
 
 main()
--- src/templates.js
+++ src/templates.js
@@ -1,3 +1,17 @@
 const engineerTemplate = engineer => {
-    
+    return ``
 }
+
+const managerTemplate = manager => {
+    const name = manager.name;
+    const id = manager.employee_id;
+    
+    return `
+<div>Manager: ${name}<div>
+<div>Manager ID ${id}<div>
+    `
+}
+
+
+
+module.exports = {engineerTemplate, managerTemplate}
